9cab73188d3c99fa43dc4f0028c1b859a9618bb9
tendotash <riannatasha17@gmail.com>
2026-09-01 03:57:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ going. Good things take time.""",
         "song_title": "our song",
         "artist": "Taylor Swift ♡",
         "audio": file_to_data_uri("assets/music/letter1.mp3"),
-        "start_time": 42,
+        "start_time": 74,
     },
 
     "2": {
